c04d32716294617370083debabaa744ed9affbb3
dschadlich <dschadlich@gmail.com>
2016-03-08 21:48:17 -0500

    got parallax working

---
+++ js/parallax.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+
+
+
+  $('.img-holder').imageScroll({
+//            image: null,
+//            imageAttribute: 'image',
+//            container: $('body'),
+//            windowObject: $(window),
+            speed:0,
+//            coverRatio:.75,
+            coverRatio:1,
+//            holderClass: 'imageHolder',
+            imgClass: 'img-holder-img hidden-xs hidden-sm',
+//            holderMinHeight: 200,
+//            holderMaxHeight: null,
+//            extraHeight: 50,
+//            mediaWidth: 1600,
+//            mediaHeight: 900,
+//            parallax: true,
+//            touch: false
+  });
+
+});
